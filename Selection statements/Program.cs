ca8dc026fb195b262d759d8d9d56eeae4b08060b
Jordan Lockwood <8xavienlockwood8@gmail.com>
2022-07-07 10:13:46 -0600

    Excercise 1

---
+++ Selection statements/Program.cs
@@ -0,0 +1,21 @@
+﻿//Exercise 1
+Console.WriteLine("Can you guess what my fav number is?");
+var userInput = int.Parse(Console.ReadLine());
+var fav = 8;
+
+if (userInput == fav)
+{
+    Console.WriteLine("Great job!!!");
+}
+else if (userInput > fav)
+{
+    Console.WriteLine("Too High!");
+}
+else if (userInput < fav)
+{
+    Console.WriteLine("Too low!");
+}
+
+//Excercise 2
+
+
