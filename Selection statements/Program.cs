205f49bd5e69cd84e9d17dc6e80b49cd177a4335
Jordan Lockwood <8xavienlockwood8@gmail.com>
2022-07-07 10:56:26 -0600

    Excersize2

--- Selection statements/Program.cs
+++ Selection statements/Program.cs
@@ -1,4 +1,5 @@
 ﻿//Exercise 1
+
 Console.WriteLine("Can you guess what my fav number is?");
 var userInput = int.Parse(Console.ReadLine());
 var fav = 8;
@@ -15,7 +16,37 @@ else if (userInput < fav)
 {
     Console.WriteLine("Too low!");
 }
+// Excersize 2
 
-//Excercise 2
+
+Console.WriteLine("What is your fav subject in school?");
+string subject = Console.ReadLine();
+switch (subject.ToLower()) 
+{
+    case "science":
+    Console.WriteLine("Science");
+        break;
+
+    case "math": 
+        Console.WriteLine("Math");
+        break;
+
+    case "gym":
+        Console.WriteLine("Gym");
+        break;
+
+    case "history": 
+        Console.WriteLine("History");
+        break;
+
+    case "shop": 
+        Console.WriteLine("Shop");
+        break;
+        
+    default: 
+        Console.WriteLine("that is not one of your classes!");
+        break;
+        
+};
 
 
