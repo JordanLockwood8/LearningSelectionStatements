03428e4634785a555b089bfb5f07049346d3322a
Jordan Lockwood <8xavienlockwood8@gmail.com>
2022-07-08 08:39:08 -0600

    adding string interpolation to my ternary opp

---
+++ Selection statements/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selection_statements
+{
+    internal class Class1
+    {
+
+    }
+}
--- Selection statements/Program.cs
+++ Selection statements/Program.cs
@@ -1,52 +1,54 @@
 ﻿//Exercise 1
 
 Console.WriteLine("Can you guess what my fav number is?");
+
 var userInput = int.Parse(Console.ReadLine());
 var fav = 8;
 
-if (userInput == fav)
-{
-    Console.WriteLine("Great job!!!");
-}
-else if (userInput > fav)
-{
-    Console.WriteLine("Too High!");
-}
-else if (userInput < fav)
-{
-    Console.WriteLine("Too low!");
-}
+    if (userInput == fav)
+    {
+        Console.WriteLine("Great job!!!");
+       
+    }
+    else if (userInput > fav)
+    {
+        Console.WriteLine("Too High!");
+    }
+    else if (userInput < fav)
+    {
+        Console.WriteLine("Too low!");
+
+    }
 // Excersize 2
+    Console.WriteLine();
+    Console.WriteLine("What is your fav subject in school?");
+    string subject = Console.ReadLine();
+    switch (subject.ToLower())
+    {
+        case "science":
+            Console.WriteLine("Science");
+            break;
 
+        case "math":
+            Console.WriteLine("Math");
+            break;
 
-Console.WriteLine("What is your fav subject in school?");
-string subject = Console.ReadLine();
-switch (subject.ToLower()) 
-{
-    case "science":
-    Console.WriteLine("Science");
-        break;
+        case "gym":
+            Console.WriteLine("Gym");
+            break;
 
-    case "math": 
-        Console.WriteLine("Math");
-        break;
+        case "history":
+            Console.WriteLine("History");
+            break;
 
-    case "gym":
-        Console.WriteLine("Gym");
-        break;
+        case "shop":
+            Console.WriteLine("Shop");
+            break;
 
-    case "history": 
-        Console.WriteLine("History");
-        break;
+        default:
+            Console.WriteLine("that is not one of your classes!");
+            break;
 
-    case "shop": 
-        Console.WriteLine("Shop");
-        break;
-        
-    default: 
-        Console.WriteLine("that is not one of your classes!");
-        break;
-        
-};
+    };
 
 
